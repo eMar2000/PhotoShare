255659ab83edaf530618880661485375764f2360
Eric Mar <emar0818@gmail.com>
2019-09-14 21:44:49 -0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,3 +1,35 @@
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1\
+T" crossorigin="anonymous">
+
+    <style>
+
+    body {
+        background-image: url("yosemite-wallpaper-8.jpg");
+        //background-color: #cccccc;
+    }
+
+    .btn-primary {
+        color: #fff;
+        background-color: #00ccff;
+        border-color: #00ccff;
+        box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.15), 0 1px 1px rgba(0, 0, 0, 0.075); }
+    .btn-primary:hover {
+        color: #fff;
+        background-color: #008fb3;
+        border-color: #008fb3; }
+
+    </style>
+
+
+</head>
+</br></br>
+<div class = "container">
+<div class = "jumbotron w-50 mx-auto">
+        <h1 class="display-4">Upload</h1>
+        </br>
 <?php
 include 'db_config.php';
 $target_dir = "uploads/";
@@ -8,7 +40,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(!isset($_POST["submit"]) || empty($_FILES["fileToUpload"]["name"])) {    
     echo "Image failed to load.</br>";
-    echo "<a href='main.php'>Return to main page</a>";
+    echo "<br><a href='main.php'>Return to main page</a>";
     exit;
 }
 // Check if file already exists
@@ -111,11 +143,15 @@ if ($uploadOk == 0) {
         $stmt->execute();
         $stmt->close();
 
-        echo "The file ".basename( $_FILES["fileToUpload"]["name"])." has been uploaded successfully.";	
+        echo "The file ".basename( $_FILES["fileToUpload"]["name"])." has been uploaded successfully.<br>";	
     } else {
         echo "Sorry, there was an error uploading your file.</br>";
     }
 }
 ?>
 
-<a href="main.php">Return to main page</a>
+<br><a href="main.php">Return to main page</a>
+</div>
+</div>
+
+</html>
